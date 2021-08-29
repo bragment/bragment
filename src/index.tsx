2e0afc08da42cc95687bfef4826692d6984576bf
Breeze <805511939@qq.com>
2021-08-29 22:05:33 +0800

    feat(public): set app icon
    - update favicon.ico
    - add page loading
    - remove react resources
    
    resolve #1

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,16 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
+  () => {
+    const overture = document.getElementById('overture');
+    setTimeout(() => {
+      overture?.classList.add('loaded');
+    }, 3000);
+    setTimeout(() => {
+      overture?.remove();
+    }, 5000);
+  }
 );
 
 // If you want to start measuring performance in your app, pass a function
