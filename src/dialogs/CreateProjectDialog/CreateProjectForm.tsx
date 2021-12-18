a654766f6533ee21b3c32ac9ef11e04c58d388eb
Breeze <805511939@qq.com>
2021-12-18 17:28:02 +0800

    feat(project): create project
    - use graphql
    - add @apollo/client & @graphql-codegen
    - create project & query projects
    - upgrade dependencies
    
    resolve #9

--- src/dialogs/CreateProjectDialog/CreateProjectForm.tsx
+++ src/dialogs/CreateProjectDialog/CreateProjectForm.tsx
@@ -1,12 +1,24 @@
-import { FormOutlined } from '@ant-design/icons';
-import { Button, Form, Input } from 'antd';
+import {
+  AlignLeftOutlined,
+  FormOutlined,
+  GlobalOutlined,
+  LockOutlined,
+} from '@ant-design/icons';
+import { Button, Form, Input, Select } from 'antd';
 import classnames from 'classnames';
-import { memo, useMemo, useState } from 'react';
+import { memo, useMemo } from 'react';
 import { useFormatMessage } from '../../components/hooks';
+import {
+  IProjectFragmentDoc,
+  ProjectVisibility,
+  useCreateProjectMutation,
+} from '../../graphql';
 import styles from './index.module.scss';
 
 interface ICreateProjectFormData {
   title: string;
+  description: string;
+  visibility: ProjectVisibility;
 }
 
 interface ICreateProjectFormProps {
@@ -14,20 +26,52 @@ interface ICreateProjectFormProps {
 }
 
 function CreateProjectForm(props: ICreateProjectFormProps) {
+  const { onFinish } = props;
   const f = useFormatMessage();
   const [form] = Form.useForm<ICreateProjectFormData>();
-  const [submitting, setSubmitting] = useState(false);
   const initialFormValues = useMemo<ICreateProjectFormData>(
     () => ({
       title: '',
+      description: '',
+      visibility: ProjectVisibility.Private,
     }),
     []
   );
+  const [addProject, { loading }] = useCreateProjectMutation({
+    update(cache, response) {
+      const a = response.data?.createProject;
+      if (a) {
+        cache.modify({
+          fields: {
+            projects(existingProjects) {
+              const newProjectRef = cache.writeFragment({
+                data: a.project,
+                fragment: IProjectFragmentDoc,
+              });
+              return {
+                ...existingProjects,
+                edges: [
+                  { node: newProjectRef, __typename: 'ProjectEdge' },
+                  ...existingProjects.edges,
+                ],
+              };
+            },
+          },
+        });
+      }
+    },
+  });
 
-  const handleSubmit = () => {
-    // const fields = form.getFieldsValue();
-    setSubmitting(true);
-    setSubmitting(false);
+  const handleSubmit = async () => {
+    const fields = form.getFieldsValue();
+    const input = {
+      fields,
+    };
+    await addProject({ variables: { input } });
+    form.resetFields();
+    if (onFinish) {
+      onFinish();
+    }
   };
 
   return (
@@ -49,12 +93,40 @@ function CreateProjectForm(props: ICreateProjectFormProps) {
           size="middle"
         />
       </Form.Item>
+      <Form.Item name="description">
+        <Input
+          autoComplete="off"
+          bordered={false}
+          className={classnames(
+            styles.fieldInput,
+            'middle-input-with-prefix-and-suffix'
+          )}
+          prefix={<AlignLeftOutlined />}
+          placeholder={f('projectDescription')}
+          size="middle"
+        />
+      </Form.Item>
+      <Form.Item name="visibility">
+        <Select
+          bordered={false}
+          className={styles.fieldSelect}
+          dropdownClassName={styles.fieldDropdown}>
+          <Select.Option value={ProjectVisibility.Private}>
+            <LockOutlined />
+            {f('private')}
+          </Select.Option>
+          <Select.Option value={ProjectVisibility.Public}>
+            <GlobalOutlined />
+            {f('public')}
+          </Select.Option>
+        </Select>
+      </Form.Item>
       <Form.Item className={styles.formActions}>
         <Button
           type="primary"
           htmlType="submit"
           onClick={handleSubmit}
-          loading={submitting}
+          loading={loading}
           size="middle">
           {f('createProject')}
         </Button>
