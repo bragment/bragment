85eb9dcaa4b8dd839bb246b50a3cd740b9036be8
Breeze <805511939@qq.com>
2022-08-12 22:02:20 +0800

    feat(components): add AnimatePing
    get more attention for the action buttons

---
+++ src/components/AnimatePing/index.tsx
@@ -0,0 +1,38 @@
+import classNames from 'classnames';
+import { memo } from 'react';
+
+export interface IAnimatePingProps {
+  children: JSX.Element;
+  ping?: boolean;
+}
+
+function AnimatePing(props: IAnimatePingProps) {
+  const { children, ping } = props;
+
+  return (
+    <span className="relative inline-flex">
+      {children}
+      {ping && (
+        <span
+          className={classNames(
+            'flex absolute h-3 w-3 top-0 right-0 -mt-[0.125rem] -mr-[0.125rem] pointer-events-none'
+          )}>
+          <span
+            className={classNames(
+              'bg-secondary',
+              'animate-ping absolute inline-flex h-full w-full rounded-full opacity-75'
+            )}
+          />
+          <span
+            className={classNames(
+              'bg-secondary-focus',
+              'relative inline-flex rounded-full h-3 w-3'
+            )}
+          />
+        </span>
+      )}
+    </span>
+  );
+}
+
+export default memo(AnimatePing);
--- src/components/CreateDataFieldButton/index.tsx
+++ src/components/CreateDataFieldButton/index.tsx
@@ -18,9 +18,7 @@ function CreateDataViewButton(props: ICreateDataViewButtonProps) {
 
   return (
     <div className={classNames('dropdown dropdown-end', className)}>
-      <label
-        tabIndex={0}
-        className={classNames('btn btn-ghost btn-sm', 'h-10 gap-2')}>
+      <label tabIndex={0} className="btn btn-ghost btn-sm">
         {children}
       </label>
       <Dropdown
